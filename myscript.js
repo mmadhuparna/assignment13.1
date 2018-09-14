ddfccf1ba1719aa36d026eff0fcfc064448d34e6
mmadhuparna <mmadhuparna@yahoo.com>
2018-09-14 11:22:17 +0530

    commit

---
+++ myscript.js
@@ -0,0 +1,28 @@
+//Given input array of strings
+var sports = ['cricket','Hockey','Tennis','Football'];
+function promise(){
+  //Create a Promise
+  var delayWork = new Promise(function (resolve, reject) {
+      //Returns the result after n seconds, where n is length of input array
+      setTimeout(resolve, sports.length*1000, {
+      //Convert the values to uppercase using map function in array    
+      uppercase: sports.map(function (x) {
+          return x.toUpperCase();
+      })
+    });
+  });
+  delayWork.then(function (data) {
+    //Resolve callback
+      console.log("Input array:")
+      console.log(sports);
+      console.log("-------------------------------------------------")
+      console.log("Output array:")
+      console.log(data);
+      document.querySelector("#pOutput").innerHTML = JSON.stringify(data);
+      
+  }).catch(function (err) {
+      //Reject callback
+      console.log(err);
+  });
+  
+}
